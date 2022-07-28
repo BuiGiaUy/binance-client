98832c294abe94474bd3e9b45521445b23adc998
BuiGiaUy <truyenbi02@gmail.com>
2022-07-28 20:05:18 +0700

    fix useEffect

--- src/auth/Provider.js
+++ src/auth/Provider.js
@@ -13,37 +13,37 @@ const Provider = ({ children }) => {
         user: null,
     });
     // Authenticate User
-    useEffect(
-        () =>
-            async function loadUser() {
-                if (localStorage[LOCAL_STORAGE_TOKEN_NAME]) {
-                    setAuthToken(localStorage[LOCAL_STORAGE_TOKEN_NAME]);
-                }
+    const loadUser = async () => {
+        if (localStorage[LOCAL_STORAGE_TOKEN_NAME]) {
+            setAuthToken(localStorage[LOCAL_STORAGE_TOKEN_NAME]);
+        }
 
-                try {
-                    const response = await axios.get(`${apiUrl}/auth`);
-                    if (response.data.success) {
-                        dispatch({
-                            type: 'SET_AUTH',
-                            payload: { isAuthenticated: true, user: response.data.user },
-                        });
-                    }
-                } catch (error) {
-                    localStorage.removeItem(LOCAL_STORAGE_TOKEN_NAME);
-                    setAuthToken(null);
-                    dispatch({ type: 'SET_AUTH', payload: { isAuthenticated: false, user: null } });
-                }
-            },
-        [],
-    );
+        try {
+            const response = await axios.get(`${apiUrl}/auth`);
+            if (response.data.success) {
+                dispatch({
+                    type: 'SET_AUTH',
+                    payload: { isAuthenticated: true, user: response.data.user },
+                });
+            }
+        } catch (error) {
+            localStorage.removeItem(LOCAL_STORAGE_TOKEN_NAME);
+            setAuthToken(null);
+            dispatch({ type: 'SET_AUTH', payload: { isAuthenticated: false, user: null } });
+        }
+    };
 
+    useEffect(() => {
+        const loadUserWrapper = () => loadUser();
+        loadUserWrapper();
+    }, []);
     // login
     const loginUser = async (userForm) => {
         try {
             const response = await axios.post(`${apiUrl}/auth/login`, userForm);
-            if (response.data.success) {
-                localStorage.setItem(LOCAL_STORAGE_TOKEN_NAME, response.data.accessToken);
-            }
+            if (response.data.success) localStorage.setItem(LOCAL_STORAGE_TOKEN_NAME, response.data.accessToken);
+
+            await loadUser();
 
             return response.data;
         } catch (error) {
